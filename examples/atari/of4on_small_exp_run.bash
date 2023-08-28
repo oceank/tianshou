bd24fe80324fb77f03afe2093fb7bab04e3d19ec
Jianhai Su <suj@email.sc.edu>
2023-08-28 17:14:06 -0400

    update the bash script for running the small experiment

--- examples/atari/of4on_small_exp_run.bash
+++ examples/atari/of4on_small_exp_run.bash
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-Task="BreakoutNoFrameskip-v4"
+Task=$1
+Tianshou_Root=/work/suj/of4on/tianshou
+SCRIPT_BASELINE=${Tianshou_Root}/examples/atari/atari_qrdqn.py
+SCRIPT_OF4ON=${Tianshou_Root}/examples/atari/atari_of4on.py
+SEEDS=(100 200 300)
 
-python atari_qrdqn.py --task ${Task} --seed 100 --eps-test 0.001 --eps-train-final 0.01 --buffer-size 1000000 --replay-buffer-min-size 50000 --lr 0.00005 --n-step 1 --target-update-freq 5000 --epoch 6 --step-per-epoch 100000 --step-per-collect 4 --update-per-step 0.25 --training-num 4 --show-progress
+for seed in "${SEEDS[@]}"
+do
+    echo -e "Run baseline with seed ${seed} in the game ${Task}"
+    python ${SCRIPT_BASELINE} --task ${Task} --seed ${seed} --eps-test 0.001 --eps-train-final 0.01 --buffer-size 1000000 --replay-buffer-min-size 50000 --lr 0.00005 --n-step 1 --target-update-freq 5000 --epoch 6 --step-per-epoch 100000 --step-per-collect 4 --update-per-step 0.25 --training-num 4
 
-python atari_qrdqn.py --task ${Task} --seed 200 --eps-test 0.001 --eps-train-final 0.01 --buffer-size 1000000 --replay-buffer-min-size 50000 --lr 0.00005 --n-step 1 --target-update-freq 5000 --epoch 6 --step-per-epoch 100000 --step-per-collect 4 --update-per-step 0.25 --training-num 4 --show-progress
+    echo -e "Run of4on with seed ${seed} in the game ${Task}"
+    python ${SCRIPT_OF4ON} --task ${Task} --seed ${seed} --offline-epoch-match-consumed-online-steps --online-epoch 6 --show-progress
 
-python atari_qrdqn.py --task ${Task} --seed 300 --eps-test 0.001 --eps-train-final 0.01 --buffer-size 1000000 --replay-buffer-min-size 50000 --lr 0.00005 --n-step 1 --target-update-freq 5000 --epoch 6 --step-per-epoch 100000 --step-per-collect 4 --update-per-step 0.25 --training-num 4 --show-progress
-
-python atari_of4on.py --task ${Task} --seed 100 --offline-epoch-match-consumed-online-steps --online-epoch 6 --show-progress
-
-python atari_of4on.py --task ${Task} --seed 200 --offline-epoch-match-consumed-online-steps --online-epoch 6 --show-progress
-
-python atari_of4on.py --task ${Task} --seed 300 --offline-epoch-match-consumed-online-steps --online-epoch 6 --show-progress
+done 
