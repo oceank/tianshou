447b3b13709cb3ed9d1a29d742828910a2235cc8
Jianhai Su <suj@email.sc.edu>
2023-09-03 04:43:17 -0400

    fix a typo

--- examples/atari/sbatch_of4on_small_exp_run.bash
+++ examples/atari/sbatch_of4on_small_exp_run.bash
@@ -48,9 +48,9 @@ Torch_Num_Threads=16 # 2, 4, 8, 16
 Train_Env_Num_Threads=4
 Test_Env_Num_Threads=5 # 5, 10
 
-#echo -e "Run baseline with seed ${seed} in the game ${Task}"
+#echo -e "Run baseline with seed ${Seed} in the game ${Task}"
 #python ${SCRIPT_BASELINE} --task ${Task} --seed ${Seed} --eps-test 0.001 --eps-train-final 0.01 --buffer-size 1000000 --replay-buffer-min-size 50000 --lr 0.00005 --n-step 1 --target-update-freq 5000 --epoch 6 --step-per-epoch 100000 --step-per-collect 4 --update-per-step 0.25 --training-num 4 --torch-num-threads ${Torch_Num_Threads} --train-env-num-threads ${Train_Env_Num_Threads} --test-env-num-threads ${Test_Env_Num_Threads}
 
-echo -e "Run of4on with seed ${seed} in the game ${Task}"
+echo -e "Run of4on with seed ${Seed} in the game ${Task}"
 python ${SCRIPT_OF4ON} --task ${Task} --seed ${Seed} --offline-epoch-match-consumed-online-steps --online-epoch 6 --torch-num-threads ${Torch_Num_Threads} --train-env-num-threads ${Train_Env_Num_Threads} --test-env-num-threads ${Test_Env_Num_Threads}
 
