415a935b3370acd263dc917f110d08e54f15fba3
Jianhai Su <suj@email.sc.edu>
2023-09-08 23:46:39 -0400

    add a trainer for of4on using dual collection, which has three types, Fixed, Scheduling and Adaptive

--- tianshou/trainer/__init__.py
+++ tianshou/trainer/__init__.py
@@ -1,6 +1,12 @@
 """Trainer package."""
 
 from tianshou.trainer.base import BaseTrainer
+from tian.trainer.offpolicy_of4on_dpc import (
+    OnlinePolicyExperienceCollectionSetting,
+    OffpolicyDualpolicyCollectionTrainer,
+    offpolicy_dualpolicycollection_trainer,
+    offpolicy_dualpolicycollection_trainer_iter,
+)
 from tianshou.trainer.offline import (
     OfflineTrainer,
     offline_trainer,
@@ -20,6 +26,10 @@ from tianshou.trainer.utils import gather_info, test_episode
 
 __all__ = [
     "BaseTrainer",
+    "OnlinePolicyExperienceCollectionSetting",
+    "offpolicy_dualpolicycollection_trainer",
+    "offpolicy_dualpolicycollection_trainer_iter",
+    "OffpolicyDualpolicyCollectionTrainer",
     "offpolicy_trainer",
     "offpolicy_trainer_iter",
     "OffpolicyTrainer",
